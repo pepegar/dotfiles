10ca33f2c45d5497ccf25dc3bf17ce0c2c4156c2
José Luis <jl.garhdez@gmail.com>
2013-06-03 13:02:50 +0300

    added path and prepend_to_path function to fish config.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,3 +6,20 @@ function :q; exit; end
 
 function ev: mvim ~/.vimrc; end
 function ef: mvim ~/.config/fish/config.fish; end
+
+function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
+    if test -d $argv[1]
+        if not contains $argv[1] $PATH
+            set -gx PATH "$argv[1]" $PATH
+        end
+    end
+end
+prepend_to_path "/usr/local/php5/bin"
+prepend_to_path "/usr/local/mysql/bin"
+prepend_to_path "/usr/local/boris/bin"
+prepend_to_path "/usr/bin"
+prepend_to_path "/bin"
+prepend_to_path "/usr/sbin"
+prepend_to_path "/sbin"
+prepend_to_path "/usr/local/bin"
+prepend_to_path "/usr/"
