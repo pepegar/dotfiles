b46f7243966d074ecb182304b0eb379758cdc9e5
José Luis García <jl.garhdez@gmail.com>
2013-06-07 22:38:18 +0300

    .

---
+++ config/fish/config.fish
@@ -0,0 +1,3 @@
+set PATH /usr/bin /bin /usr/sbin /sbin /usr/local/bin /usr/local/share/npm/bin /usr/X11R6/bin /Users/Pepe/phpctags /usr/local/Cellar/mysql/5.5.27/bin /usr/local/php5/bin $PATH
+
+function gvim; mvim $argv; end)
