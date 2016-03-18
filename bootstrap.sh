140e40519855919d63a63f59515b8b095311cade
Pepe García <jl.garhdez@gmail.com>
2016-03-18 13:05:02 +0100

    add powerline

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,4 +17,6 @@ ln -s ~/dotfiles/sqliterc ~/.sqliterc
 ln -s ~/dotfiles/vkgrc ~/.vkgrc
 ln -s ~/dotfiles/.spacemacs ~/.spacemacs
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+ln -s ~/dotfiles/powerline ~/powerline
 echo "-------- done! ---------"
