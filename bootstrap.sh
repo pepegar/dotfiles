0c96fbd68fe215abedc1afa88d95c49efb58950d
pepegar <jl.garhdez@gmail.com>
2017-03-01 16:58:06 +0100

    mooooooooore

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,17 +6,8 @@ echo "updating submodules"
 git submodule update
 
 echo "creating symlinks"
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/vim/vimrc ~/.nvimrc
 ln -s ~/dotfiles/config ~/.config
-ln -s ~/dotfiles/vimperator ~/.vimperator
-ln -s ~/dotfiles/vimperatorrc ~/.vimperatorrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/sqliterc ~/.sqliterc
-ln -s ~/dotfiles/vkgrc ~/.vkgrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/emacs.d ~/.emacs.d
-ln -s ~/dotfiles/powerline ~/powerline
 echo "-------- done! ---------"
